f1fcd9e9dac83e88dff636db1ff25bb6f5374a28
Alexandre-Caldeira <alexandre.caldeira@ufv.br>
2024-06-19 22:28:13 -0300

    change emojis for better compatibility

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const targetDate = new Date('2024/06/17');
+    const targetDate = new Date('2024/06/19');
     const today = new Date();
-    let daysDifference = Math.ceil((today - targetDate) / (1000 * 60 * 60 * 24));
+    let daysDifference = Math.ceil((today - targetDate) / (1000 * 60 * 60 * 24))-1;
     
-    document.getElementById('days').innerText = `🌈${daysDifference.toString().padStart(10, ' ')}🫧`; // Preenche com espaços para 10 dígitos
+    let displayText;
+    if (daysDifference === 0) {
+        displayText = `💀${daysDifference}📉`;
+    } else {
+        displayText = `🌈${daysDifference.toString().padStart(10, ' ')}📈`;
+    }
+    
+    document.getElementById('days').innerText = displayText;
     
     const dateInfoElement = document.getElementById('dateInfo');
     const formattedDate = `${targetDate.getDate()}/${targetDate.getMonth() + 1}/${targetDate.getFullYear()}`; // Formata a data
